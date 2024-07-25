c6dae0bf66ef5969c5ef277dd6c623a3c1c4dac5
Stratis Maistros <stratismaistros@gmail.com>
2024-07-25 10:36:34 +0300

    Upgrade from IBM BAMOE 9.0.x to IBM BAMOE 9.1.x

--- service/src/main/java/org/acme/service/HelloService.java
+++ service/src/main/java/org/acme/service/HelloService.java
@@ -1,8 +1,8 @@
 package org.acme.service;
 
 import java.util.List;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
 import org.acme.Lib1Unit;
 import org.acme.Lib2Unit;
 import org.drools.ruleunits.api.RuleUnitInstance;
